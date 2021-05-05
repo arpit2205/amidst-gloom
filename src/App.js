df92bef79728344bba3990adf243f6576a15cdd5
Arpit Gupta <arpit.8.a@gmail.com>
2021-05-05 20:39:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.scss";
+import React, { useEffect } from "react";
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
+  useEffect(() => {
+    gsap.utils.toArray(".panel").forEach((panel, i) => {
+      ScrollTrigger.create({
+        trigger: panel,
+        start: "top top",
+        pin: true,
+        pinSpacing: false,
+      });
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="panel one">
+        <h1 style={{ color: "white" }}>HI</h1>
+      </div>
+      <div className="panel two">
+        <h1 style={{ color: "white" }}>I AM</h1>
+      </div>
+      <div className="panel three">
+        <h1 style={{ color: "white" }}>ARPIT</h1>
+      </div>
     </div>
   );
 }
